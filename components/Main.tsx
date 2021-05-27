347124af8207bb2772a53b10c4fa24b559e4ebba
Michael Stubbs <michaelstubbsdev@gmail.com>
2021-05-26 17:10:52 -0700

    add types

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,20 +1,23 @@
 import { useState } from "react";
 import Carousel from "../components/Carousel";
 import RoomText from "../components/RoomText";
+import styles from "../styles/Main.module.scss";
 
-interface MProps {
+interface MainProps {
   items: Array<Object>;
 }
 
-function Main(props: MProps) {
-  const items = props.items;
+function Main(props: MainProps) {
+  const items: Array<Object> = props.items;
 
   const [slide, setSlide] = useState(0);
+
+  let current: any = items[slide];
 
   return (
     <main>
       <Carousel items={items} setSlide={setSlide} />
-      <RoomText name={items[slide].name} desc={items[slide].desc} />
+      <RoomText name={current.name} desc={current.desc} />
     </main>
   );
 }
--- components/RoomText.tsx
+++ components/RoomText.tsx
@@ -1,11 +1,11 @@
 import styles from "../styles/Main.module.scss";
 
-interface RProps {
+interface RoomProps {
   name: string;
   desc: string;
 }
 
-function RoomText(props: RProps) {
+function RoomText(props: RoomProps) {
   return (
     <div className={styles.roomText}>
       <h2>{props.name}</h2>
