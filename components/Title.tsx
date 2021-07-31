eb1e78ac54aa1c5d0be502f4085a1961bde05c59
Michael Stubbs <michaelstubbsdev@gmail.com>
2021-07-30 19:18:10 -0700

    add final design changes

--- components/Title.tsx
+++ components/Title.tsx
@@ -5,6 +5,13 @@ function Header() {
   return (
     <header className={styles.header}>
       <h1 className={styles.title}>Doki Design</h1>
+      <img
+        src="/circle-cropped.png"
+        alt=""
+        width="100px"
+        height="100px"
+        className={styles.logo}
+      />
       <SocialGroup />
     </header>
   );
